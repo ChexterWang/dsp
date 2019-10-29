942157dce0dc4c0f7eed04f7614acbe28cfd6da3
ChexterWang@Arch <tos515541905@gmail.com>
2019-10-29 11:36:41 +0800

    need fix: train; under construction: test, delta

--- hw1/src/test.c
+++ hw1/src/test.c
@@ -1,6 +1,8 @@
 //     ./test <models_list_path> <seq_path> <output_result_path>
 // ex: ./test ./modellist.txt ./data/test_seq.txt ./result.txt
-// ex: ./test ./test_lbl.txt ./data/test_seq.txt ./result.txt
+//
+// test: put every line in test_seq.txt into 5 models and output
+// the best fitting model of that line and its probability/
 //
 // ./result.txt
 //     <trained_model_path> <max P(O|trained_model, state_sequence)>
@@ -9,11 +11,61 @@
 
 #include <iostream>
 #include <fstream>
+
+#define MAX_MODELS 5
+#define MAX_SEQ 2500
+#define SEQ_LENGTH 50
+#define MAX_STATE 6
+
 #include "../inc/hmm.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-   cout << argc << argv[0] << argv[1] << endl;
-   return 0;
+
+   // argument exceptions handling
+   if(argc != 4){
+      cerr << "Error: Wrong ammount of arguments." << endl;
+      exit(-1);
+   }
+   ifstream* modelList = new ifstream(argv[1]);
+   if(!modelList->is_open()){
+      cerr << "Error: Can't open model list file. (" << argv[1] << ")";
+      exit(-1);
+   }
+   char(*models)[256] = new char[MAX_MODELS][256];
+   for(int i = 0; i < MAX_MODELS; ++i){
+      modelList->getline(models[i], 256);
+   }
+   modelList->close();
+
+   ifstream* testSeq = new ifstream(argv[2]);
+   if(!testSeq->is_open()){
+      cerr << "Error: Can't open test sequence file. (" << argv[2] << ")";
+      exit(-1);
+   }
+   char(*seqs)[SEQ_LENGTH+1] = new char[MAX_SEQ][SEQ_LENGTH+1];
+   for(int i = 0; i < MAX_SEQ; ++i){
+      testSeq->getline(seqs[i], SEQ_LENGTH+1);
+   }
+   testSeq->close();
+
+   double* p = new double[MAX_SEQ];
+   char(*result)[256] = new char[MAX_SEQ][256];
+   HMM* hmm = new HMM[MAX_MODELS];
+   load_models(argv[1], hmm, MAX_MODELS);
+
+   // TODO: test
+   for(int current_seq = 0; current_seq < MAX_SEQ; ++current_seq){
+      char(*delta)[MAX_STATE] = new char[SEQ_LENGTH][MAX_STATE];
+      for(int s = 0; s < MAX_STATE; ++s){
+         break;
+      }
+   }
+
+   ofstream* resultFile = new ofstream(argv[3]);
+   for(int i = 0; i < MAX_SEQ; ++i){
+      *resultFile << result[i] << " " << p[i] << endl;
+   }
+
 }
--- hw1/src/train.c
+++ hw1/src/train.c
@@ -38,10 +38,10 @@ int main(int argc, char const *argv[]) {
       cerr << "Error: Can't open train sequence file.";
       exit(-1);
    }
-   char** seqs = new char*[MAX_SEQ];
+   char(*seqs)[SEQ_LENGTH] = new char[MAX_SEQ][SEQ_LENGTH]{};
    for(int i = 0; i < MAX_SEQ; ++i){
-      seqs[i] = new char[SEQ_LENGTH];
       trainSeq->getline(seqs[i], SEQ_LENGTH+1);
+      // cout << trainSeq->bad() << endl;
    }
    trainSeq->close();
    delete trainSeq;
@@ -50,87 +50,87 @@ int main(int argc, char const *argv[]) {
    HMM* hmm_initial = new HMM();
    loadHMM( hmm_initial, argv[2] );
 
-   // declare forward/backward etc. variables
-   double** alpha   = new double*[SEQ_LENGTH];
-   double** beta    = new double*[SEQ_LENGTH];
-   double* gamma    = new double[MAX_STATE]{};
-   double** epsilon = new double*[MAX_STATE];
-   for(int i = 0; i < SEQ_LENGTH; ++i){
-      alpha[i]   = new double[MAX_STATE];
-      beta[i]    = new double[MAX_STATE];
-      if(i < MAX_STATE) epsilon[i] = new double[MAX_STATE]{};
-   }
 
    for(int i = 0; i<iter; ++i){
+
       // run every iteration
+      double alpha[SEQ_LENGTH][MAX_STATE]{};
+      double beta[SEQ_LENGTH][MAX_STATE]{};
+      double epsilon[MAX_STATE][MAX_STATE]{};
+      double gamma[MAX_STATE]{};
       double pi[MAX_STATE]{};
       double b[MAX_OBSERV][MAX_STATE]{};
+
       // run every sequences
       for(int current_seq = 0; current_seq < MAX_SEQ; ++current_seq){
 
-         double p = 0;
-
          // TODO: initializtion: beta
          // initialization: alpha, beta
-         for(int current_state = 0; current_state < MAX_STATE; ++current_state){
-            alpha[0][current_state] = hmm_initial->initial[current_state]
-                                      *hmm_initial->observation[seqs[current_seq][0]-'A'][current_state];
-            beta[SEQ_LENGTH-1][current_state] = 1;
+         int c = seqs[current_seq][0]-'A';
+         for(int i = 0; i < MAX_STATE; ++i){
+            alpha[0][i] = hmm_initial->initial[i]*hmm_initial->observation[c][i];
+            beta[SEQ_LENGTH-1][i] = 1;
          }
 
          // TODO: induction, termination: beta
          // induction, termination: alpha, beta
-         for(int current_ob = 1; current_ob < SEQ_LENGTH; ++current_ob){
-            for(int current_state = 0; current_state < MAX_STATE; ++current_state){
+         for(int t = 1; t < SEQ_LENGTH; ++t){
+            c = seqs[current_seq][t-1]-'A';
+            int d = seqs[current_seq][SEQ_LENGTH-t]-'A';
+            int e = SEQ_LENGTH-t;
+            int g = SEQ_LENGTH-1-t;
+            for(int j = 0; j < MAX_STATE; ++j){
                double sum_alpha = 0, sum_beta = 0;
-               for(int s = 0; s < MAX_STATE; ++s){
-                  sum_alpha += alpha[current_ob-1][s]*hmm_initial->transition[s][current_state];
-                  sum_beta += beta[SEQ_LENGTH-current_ob][s]
-                              *hmm_initial->transition[current_state][s]
-                              *hmm_initial->observation[seqs[current_seq][SEQ_LENGTH-current_ob]-'A'][s];
+               for(int i = 0; i < MAX_STATE; ++i){
+                  sum_alpha += alpha[t-1][i]*hmm_initial->transition[i][j];
+                  sum_beta += beta[e][i]
+                              * hmm_initial->transition[j][i]
+                              * hmm_initial->observation[d][i];
                }
-               alpha[current_ob][current_state] = sum_alpha*hmm_initial->observation[seqs[current_seq][current_ob]-'A'][current_state];
-               beta[SEQ_LENGTH-1-current_ob][current_state] = sum_beta;
+               alpha[t][j] = sum_alpha*hmm_initial->observation[c][j];
+               beta[g][j] = sum_beta;
             }
          }
 
          // TODO: create: gamma, epsilon
          // p = P(O|Model)
+         double p = 0;
+         c = SEQ_LENGTH-1;
          for(int current_state = 0; current_state < MAX_STATE; ++current_state){
-            p += alpha[SEQ_LENGTH-1][current_state];
+            p += alpha[c][current_state];
          }
          // for gamma, epsilon
          for(int current_ob = 0; current_ob < SEQ_LENGTH-1; ++current_ob){
+            c = seqs[current_seq][current_ob+1]-'A';
+            int d = current_ob+1;
+            int e = seqs[current_seq][current_ob]-'A';
             for(int current_state = 0; current_state < MAX_STATE; ++current_state){
-               gamma[current_state] += alpha[current_ob][current_state]
-                                       * beta[current_ob][current_state]
-                                       / p;
-               b[seqs[current_seq][current_ob]-'A'][current_state] += alpha[current_ob][current_state]
-                                                                      * beta[current_ob][current_state]
-                                                                      / p;
-               if(current_ob == 0) pi[current_state] += alpha[current_ob][current_state]
-                                                        * beta[current_ob][current_state]
-                                                        / p;
+               double GAMMA = alpha[current_ob][current_state]*beta[current_ob][current_state]/p;
+               gamma[current_state] += GAMMA;
+               b[e][current_state] += GAMMA;
+               if(current_ob == 0) pi[current_state] += GAMMA;
                for(int s = 0; s < MAX_STATE; ++s){
                   epsilon[current_state][s] += alpha[current_ob][current_state]
                                                * hmm_initial->transition[current_state][s]
-                                               * hmm_initial->observation[seqs[current_seq][current_ob+1]-'A'][s]
-                                               * beta[current_ob+1][s]
+                                               * hmm_initial->observation[c][s]
+                                               * beta[d][s]
                                                / p;
                }
             }
          }
          // for last gamma
-         for(int current_state = 0; current_state < MAX_STATE; ++current_state){
-            gamma[current_state] += alpha[SEQ_LENGTH-1][current_state]
-                                    * beta[SEQ_LENGTH-1][current_state]
-                                    / p;
-         }
+         // c = SEQ_LENGTH-1;
+         // for(int current_state = 0; current_state < MAX_STATE; ++current_state){
+         //    gamma[current_state] += alpha[c][current_state]
+         //                            * beta[c][current_state]
+         //                            / p;
+         // }
 
       }
       for(int current_state = 0; current_state < MAX_STATE; ++current_state){
          hmm_initial->initial[current_state] = pi[current_state] / MAX_SEQ;
          for(int s = 0; s < MAX_STATE; ++s){
+            //cout << epsilon[current_state][s] << ", " << gamma[current_state] << endl;
             hmm_initial->transition[current_state][s] = epsilon[current_state][s] / gamma[current_state];
          }
          for(int o = 0; o < MAX_OBSERV; ++o){
